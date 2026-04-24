c3561598de90d52ede67a692bacf757a8ba9adaa
HoupIlthinkft <shleakhter@gmail.com>
2026-04-24 21:55:03 +0300

    сдедал фильтрацию и просмотр таска

--- src/scheduleCustom.tsx
+++ src/scheduleCustom.tsx
@@ -8,8 +8,15 @@ export function ScheduleCustomComponent(props) {
 
     const [rangeDate, setRangeDate] = useState([new Date(), new Date().fp_incr(7)]);
 
-    const tasksCustom = props.tasksCustom
+    const [sortingMenuOpen, setSortingMenuOpen] = useState(false);
+    
+    const [sorting, setSorting] = useState("unSorted")
+    const [sortedHigh, setSortedHigh] = useState(true);
+    const [sortedMedium, setSortedMedium] = useState(true);
+    const [sortedLow, setSortedLow] = useState(true);
 
+    const tasksCustom = props.tasksCustom
+    
     useEffect(() => {
         if (rangeDate != null) getTasksForCustomRange(rangeDate);
     }, [rangeDate]);
@@ -19,10 +26,10 @@ export function ScheduleCustomComponent(props) {
             <div className="flex flex-row justify-between items-end">
                 <p className="font-semibold text-header text-[clamp(2rem,5vw,5rem)]">Входящие задачи</p>
                 <div className="flex flex-col gap-[clamp(5px,2vh,20px)]">
-                    <div className="flex flex-row gap-[clamp(5px,1vw,20px)] font-semibold text-[clamp(1rem,2vw,2rem)] w-fit bg-plate-muted px-[clamp(5px,1vw,20px)] py-[clamp(5px,1vh,10px)] rounded-[10px] hover:bg-plate-nav hover:text-hover-nav hover:scale-110 transition-all duration-300 ease-in">
+                    <div className="flex flex-row gap-[clamp(5px,1vw,20px)] font-semibold text-[clamp(1rem,2vw,2rem)] cursor-pointer w-fit bg-plate-muted px-[clamp(5px,1vw,20px)] py-[clamp(5px,1vh,10px)] rounded-[10px] hover:bg-plate-nav hover:text-hover-nav hover:scale-110 transition-all duration-300 ease-in">
                         <span className="material-symbols-outlined self-center h-fit scale-[2]">date_range</span>
                         <Flatpickr 
-                            className="text-center outline-none"
+                            className="text-center outline-none cursor-pointer"
                             options={{ 
                                 mode: "range",
                                 minDate: "today",
@@ -37,21 +44,79 @@ export function ScheduleCustomComponent(props) {
                             }}
                         />
                     </div>
-                    <div className="flex flex-row gap-[clamp(5px,1vw,20px)] bg-plate-muted px-[clamp(5px,1vw,20px)] py-[clamp(5px,1vh,10px)] rounded-[10px] hover:bg-plate-nav hover:text-hover-nav hover:scale-110 transition-all duration-300 ease-in">
-                        <span className="material-symbols-outlined self-center h-fit scale-[2]">filter_alt</span>
-                        <p className="font-semibold text-center w-[100%] text-[clamp(1rem,2vw,2rem)]">Фильтрация</p>
+                    <div className="flex flex-col gap-[clamp(5px,1vh,10px)] bg-plate-muted px-[clamp(5px,1vw,20px)] py-[clamp(5px,1vh,10px)] rounded-[10px] hover:bg-plate-nav hover:text-hover-nav hover:scale-110 transition-all duration-300 ease-in">
+                        <div onClick={() => setSortingMenuOpen(!sortingMenuOpen)} className="flex flex-row gap-[clamp(5px,1vw,20px)] cursor-pointer">
+                            <span className="material-symbols-outlined self-center h-fit scale-[2]">filter_alt</span>
+                            <p className="font-semibold text-center w-[100%] text-[clamp(1rem,2vw,2rem)]">Фильтрация</p>
+                        </div>
+                        {
+                            sortingMenuOpen ? (
+                                <div className="flex flex-col gap-[clamp(5px,1vh,10px)]">
+                                    <div onClick={() => {
+                                        sorting == "sorted" ? setSorting("revSorted") : (
+                                                                                    sorting == "revSorted" ? setSorting("unSorted") : setSorting("sorted"))
+                                    }} className="flex flex-row gap-[clamp(5px,0.5vw,10px)] cursor-pointer">
+                                        <span className={`material-symbols-outlined ${sorting == "revSorted" ? "scale-[-2]" : "scale-[2]"} self-center`}>{
+                                            sorting == "sorted" || sorting == "revSorted" ? "sort" : "menu"
+                                        }</span>
+                                        <p className="font-semibold text-[clamp(1rem,2vw,2rem)]">Приоритет</p>
+                                    </div>
+                                    <div className="flex flex-col gap-[5px,1vh,10px]">
+                                        <div onClick={() => setSortedHigh(!sortedHigh)} className="flex flex-row gap-[clamp(10px,2vw,30px)] justify-between cursor-pointer">
+                                            <p className="font-medium text-[clamp(0.75rem,1.5vw,2rem)]">Высокий</p>
+                                            <span className="material-symbols-outlined scale-[2] self-center">{sortedHigh ? "select_check_box" : "check_box_outline_blank"}</span>
+                                        </div>
+                                        <div onClick={() => setSortedMedium(!sortedMedium)} className="flex flex-row gap-[clamp(10px,2vw,30px)] justify-between cursor-pointer">
+                                            <p className="font-medium text-[clamp(0.75rem,1.5vw,2rem)]">Средний</p>
+                                            <span className="material-symbols-outlined scale-[2] self-center">{sortedMedium ? "select_check_box" : "check_box_outline_blank"}</span>
+                                        </div>
+                                        <div onClick={() => setSortedLow(!sortedLow)} className="flex flex-row gap-[clamp(10px,2vw,30px)] justify-between cursor-pointer">
+                                            <p className="font-medium text-[clamp(0.75rem,1.5vw,2rem)]">Низкий</p>
+                                            <span className="material-symbols-outlined scale-[2] self-center">{sortedLow ? "select_check_box" : "check_box_outline_blank"}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            ) : ""
+                        }
                     </div>
                 </div>
             </div>
             <div className="grid grid-cols-3 overflow-y-auto max-h-[60vh] bg-plate-accent rounded-[15px] px-[clamp(5px,2vw,40px)] py-[clamp(5px,4vh,40px)] gap-y-[clamp(5px,3vh,30px)] gap-x-[clamp(5px,2vw,30px)]">
                 {
-                    tasksCustom.map((day) => (
-                        day.tasks.map((el, index) => (
-                            <TaskComponent key={index} header={el.header} content={el.content} priority={el.priority} deadline={`${day.date} ${el.deadline}`}/>
+                    sortingTask(
+                    tasksCustom.flatMap((day, indexTop) => (
+                        day.tasks.map((el, indexBottom) => (
+                            sortedHigh && "high" == el.priority.toLowerCase() || sortedMedium && "medium" == el.priority.toLowerCase() || sortedLow && "low" == el.priority.toLowerCase() ? (
+                            <TaskComponent key={String(indexTop) + String(indexBottom)} header={el.header} content={el.content} priority={el.priority} deadline={`${day.date} ${el.deadline}`}/>
+                            ) : ""
                         ))
-                    ))
+                    )), sorting)
                 }
             </div>
         </div>
     )
+}
+
+
+function sortingTask(tasks, rule : "sorted" | "revSorted" | "unSorted") {
+    if (rule == "unSorted") return tasks;
+    else {
+        let low : number = 0;
+        let mid : number = 0;
+        let high : number = tasks.length - 1;
+        
+        while (mid <= high) {
+            if (tasks[mid]?.props?.priority == "low") {
+                tasks[low], tasks[mid] = tasks[mid], tasks[low];
+                low += 1;
+                mid += 1;
+            }
+            else if (tasks[mid]?.props?.priority == "medium") mid += 1;
+            else {
+                [tasks[mid], tasks[high]] = [tasks[high], tasks[mid]];
+                high -= 1;
+            }
+        }
+        return rule == "sorted" ? tasks.reverse() : tasks;
+    }
 }
--- src/task.tsx
+++ src/task.tsx
@@ -1,12 +1,33 @@
-export function TaskComponent(props) {
+import { useState } from "react";
+
+export function TaskComponent(props : {header : string, content : string, priority : "high" | "medium" | "low", deadline : string}) {
+    const [viewTask, setViewTask] = useState(false);
+
     return (
-        <div className={`${props.className} flex flex-col gap-[clamp(6px,2vh,20px)] px-[clamp(8px,1vw,20px)] py-[clamp(10px,3vh,30px)] bg-plate-task rounded-[12px]`}>
+        <div onClick={() => setViewTask(!viewTask)} className={'flex flex-col gap-[clamp(6px,2vh,20px)] cursor-pointer px-[clamp(8px,1vw,20px)] py-[clamp(10px,3vh,30px)] bg-plate-task rounded-[12px]'}>
             <h2 className="font-semibold text-task-header px-[clamp(5px,1vw,20px)] text-[clamp(0.5rem,2vw,1.5rem)]">{props.header}</h2>
             <p className="font-medium line-clamp-5 text-[clamp(0.25rem,1vw,1rem)] text-task-content">{props.content}</p>
             <div className="flex flex-row justify-between">
                 <p className={`w-min text-priority-${props.priority} font-medium text-[clamp(0.5rem,1vw,1rem)]`}>{`Приориет: ${props.priority}`}</p>
                 <p className="w-min font-medium text-[clamp(0.5rem,1vw,1rem)] text-task-content">{`Дедлайн: ${props.deadline}`}</p>
             </div>
+                {
+                    viewTask ? (
+                        <div className="absolute cursor-default w-[100vw] h-[100vh] top-[0%] left-[0%] flex items-center justify-center bg-black/66">
+                            <div className="flex flex-col w-fit max-w-[60vw] h-fit gap-[clamp(5px,4vh,40px)] px-[clamp(10px,2vw,30px)] py-[clamp(10px,3vh,30px)] self-center justify-self-center bg-plate-task rounded-[12px]">
+                                <span onClick={() => setViewTask(!viewTask)} className="material-symbols-outlined cursor-pointer self-end scale-[2] hover:text-hover-nav hover:scale-210 transition-all duration-250 ease-in">close</span>
+                                <div className="flex flex-col gap-[clamp(5px,4vh,40px)] h-[100%] justify-between">
+                                    <h2 className="font-semibold text-task-header px-[clamp(5px,1vw,20px)] text-[clamp(0.5rem,2vw,2rem)]">{props.header}</h2>
+                                    <p className="font-medium text-[clamp(0.5rem,1.5vw,1.5rem)] text-task-content max-h-[40vh] overflow-y-auto">{props.content}</p>
+                                    <div className="flex flex-row justify-between">
+                                        <p className={`w-min text-priority-${props.priority} font-medium text-[clamp(0.5rem,1vw,1rem)]`}>{`Приориет: ${props.priority}`}</p>
+                                        <p className="w-min font-medium text-[clamp(0.5rem,1vw,1rem)] text-task-content">{`Дедлайн: ${props.deadline}`}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : ""
+                }
         </div>
     )
 }
